8f8bf361d0d3672a997e6493692058b4b9c78a7a
student <student@192.168.52.57>
2018-04-23 11:31:17 +0800

    task1 done

--- src/main/java/ouhk/comps380f/controller/TicketUserController.java
+++ src/main/java/ouhk/comps380f/controller/TicketUserController.java
@@ -2,13 +2,15 @@ package ouhk.comps380f.controller;
 
 import java.io.IOException;
 import javax.annotation.Resource;
+import javax.swing.text.View;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
 import ouhk.comps380f.dao.TicketUserRepository;
 import ouhk.comps380f.model.TicketUser;
@@ -19,6 +21,9 @@ public class TicketUserController {
 
     @Resource
     TicketUserRepository ticketUserRepo;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @RequestMapping(value = {"", "list"}, method = RequestMethod.GET)
     public String list(ModelMap model) {
@@ -64,9 +69,9 @@ public class TicketUserController {
     }
 
     @RequestMapping(value = "create", method = RequestMethod.POST)
-    public View create(Form form) throws IOException {
+    public RedirectView create(Form form) throws IOException {
         TicketUser user = new TicketUser(form.getUsername(),
-                form.getPassword(),
+                passwordEncoder.encode(form.getPassword()),
                 form.getRoles()
         );
         ticketUserRepo.save(user);
@@ -74,7 +79,7 @@ public class TicketUserController {
     }
 
     @RequestMapping(value = "delete/{username}", method = RequestMethod.GET)
-    public View deleteTicket(@PathVariable("username") String username) {
+    public RedirectView deleteTicket(@PathVariable("username") String username) {
         ticketUserRepo.delete(ticketUserRepo.findOne(username));
         return new RedirectView("/user/list", true);
     }
